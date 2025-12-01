5736a0d3b842f7b51b25eb9fc0f47df89ebaa29a
amandineuwd <amandine.underwood@gmail.com>
2025-12-01 11:09:40 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = Boolean(process.env.GITHUB_ACTIONS);
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  assetPrefix,
+  basePath,
 };
 
 export default nextConfig;
